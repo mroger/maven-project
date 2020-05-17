9c3846e1e381fbb56709c3f43064e13d75195290
Marcos R Oliveira <mroger.oliveira@gmail.com>
2020-05-17 18:14:14 -0300

    Changing version

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,6 +18,6 @@ public class Greeter {
    * @return greet string
    */
   public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello, %s! Version 3", someone);
   }
 }
